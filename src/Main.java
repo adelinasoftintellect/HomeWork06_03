202544505b7fee1851d054190bb725da23c37bad
Adi <adelina_yordanova11@abv.bg>
2016-05-30 18:34:08 +0300

    HomeWork06_03/04.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 class Stock {
 
 	@Override
@@ -29,32 +31,73 @@ class Stock {
 	public void setAvailability(boolean availability) {
 		this.availability = availability;
 	}
-	public void all(){
+
+	public void all() {
 		System.out.println("Price is " + this.price);
 		System.out.println("Availability " + this.availability);
+	}
+}
+
+class Store {
+	private String address;
+	private String name;
+	private Vector<Stock> stocks = new Vector<Stock>();
+
+	public Store(String address, String name) {
+		super();
+		this.address = address;
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Store() {
+		this("", "");
+	}
+
+	public void store() {
+		System.out.println("Store address is: " + this.address);
+		System.out.println("Store name is: " + this.name);
+	}
+
+	public void add(Stock stock) {
+		stocks.add(stock);
 	}
 }
 
 public class Main {
 
 	public static void main(String[] args) {
-		Meat chicken = new Meat (9,true,"Chicken", 2);
+		Meat chicken = new Meat(9, true, "Chicken", 2);
 		chicken.all();
 		chicken.food();
-		Vegetables potatoes = new Vegetables (3,true,true,false);
+		Vegetables potatoes = new Vegetables(3, true, true, false);
 		potatoes.all();
 		potatoes.vegs();
 		Fruits apple = new Fruits(1, false, false, "green");
 		apple.all();
 		apple.fruit();
-		
+
 		Drinks coffee = new Drinks(2, true, false, true);
 		coffee.all();
 		coffee.drink();
 		Desserts brulee = new Desserts(4, true, true, false);
 		brulee.all();
 		brulee.dessert();
-		
 
 	}
 }
